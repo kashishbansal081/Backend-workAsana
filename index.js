0ef8955e3fdf422dee383be614767b88de0f1ca0
Kashish Bansal <Kashishbansal081@gmail.com>
2026-06-04 09:34:40 +0530

    cors issue fixes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const jwt = require("jsonwebtoken");
 const saltrounds = 12;
 
 const corOptions = {
-  origin: "https://backend-work-asana.vercel.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
