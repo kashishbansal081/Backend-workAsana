b04aa4b57b1421bcae3ed4c438a6a435c0ade18f
Kashish Bansal <Kashishbansal081@gmail.com>
2026-06-06 09:40:05 +0530

    added logs to db file

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -4,17 +4,29 @@ require("dotenv").config();
 const DB = process.env.MONGODB_URL;
 
 const dbConnect = async () => {
-  console.log('Connecting to DB...');
-  await mongoose
-    .connect(DB, {
+  try {
+    console.log("Connecting to MongoDB...");
+
+    await mongoose.connect(DB, {
       serverSelectionTimeoutMS: 30000,
-    })
-    .then(() => {
-      console.log("DB connected");
-    })
-    .catch((error) => {
-      console.log(`${error} while connecting with db`);
     });
+
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+  }
 };
+
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB Connected");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB Disconnected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("MongoDB Error:", err);
+});
 
 module.exports = dbConnect;
